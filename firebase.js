08b2b727b164ba0e1eff6b1ece9d11c108046d6c
ashish gupta <ashishgupta@ashishs-MacBook-Air.local>
2025-07-18 16:13:11 +0530

    removed secrets

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBBZBFNGktHNoK1KS315CKBs_bP4TTwWpc",
-    authDomain: "dentally-e4739.firebaseapp.com",
-    projectId: "dentally-e4739",
-    storageBucket: "dentally-e4739.firebasestorage.app",
-    messagingSenderId: "753472120085",
-    appId: "1:753472120085:web:f50c61b58654212133f51c",
-    measurementId: "G-EC95QYFJN4"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
